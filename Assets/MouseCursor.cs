b4971009d98dba1fc7e185a3a3bdba7d3fc539a5
RyoMaeda2525 <maetyuryu@gmail.com>
2021-10-20 15:22:51 +0900

    Unitychanを移動できるようにした,フリールックカメラ,マウスホイールで視野角変更

---
+++ Assets/MouseCursor.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class MouseCursor : MonoBehaviour
+{
+    CinemachineFreeLook cinema = default; //シネマシーンを取得
+    bool onCursor = false; //マウスカーソルを制御する
+    // Start is called before the first frame update
+    void Start()
+    {
+        cinema = GetComponent<CinemachineFreeLook>();
+        OnEnable();//ゲーム開始時マウスカーソルを消す
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (onCursor)//マウスカーソルを消す処理
+        {
+            Cursor.visible = false;
+        }
+        else //マウスカーソルを出す
+        {
+            Cursor.visible = true;
+        }
+        
+        float y = Input.GetAxis("Mouse ScrollWheel"); //視野角をマウスホイールで変える
+        if(y > 0)
+        {  if(cinema.m_Lens.FieldOfView <= 99.5f)
+            {
+                cinema.m_Lens.FieldOfView += 1.5f;
+            }
+        }
+        else if (y < 0)
+        {
+            if (cinema.m_Lens.FieldOfView >= 59.5f)
+            {
+                cinema.m_Lens.FieldOfView -= 1.5f;
+            }
+        }
+
+    }
+
+    void OnEnable()
+    {
+        // マウスカーソルを消すには、以下の行をアンコメントする
+        //Cursor.visible = false;
+        onCursor = true;
+    }
+
+    void OnDisable()
+    {
+        Cursor.visible = true;
+    }
+}
---
+++ Assets/Scripts/PauseMenuController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PauseMenuController : MonoBehaviour
+{
+    static bool _commandFlg = false;
+    public event Action<bool> OnCommandMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 左クリックしたら一時停止・再開しつつコマンドメニューを切り替える
+        if (Input.GetButtonDown("Fire1"))
+        {
+            CommandMenu();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CommandMenu()
+    {
+        _commandFlg = !_commandFlg;
+        OnCommandMenu(_commandFlg);  // これで変数に代入した関数を（全て）呼び出せる
+    }
+}
